617955b91e46eb802fb45ec08775d3658fbb718e
DilipKumar <diliptilonia@gmail.com>
2025-02-28 07:19:54 +0100

    first commit
    intro

---
+++ codereview/codereview/codereviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  codereviewApp.swift
+//  codereview
+//
+//  Created by Dilip Gurjar on 28.02.25.
+//
+
+import SwiftUI
+
+@main
+struct codereviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ codereview/codereviewTests/codereviewTests.swift
@@ -0,0 +1,17 @@
+//
+//  codereviewTests.swift
+//  codereviewTests
+//
+//  Created by Dilip Gurjar on 28.02.25.
+//
+
+import Testing
+@testable import codereview
+
+struct codereviewTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
